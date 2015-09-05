f3c9fbc597465c263cf4a59b0fa4220c44958af6
AnqurVanillapy <anqurvanillapy@gmail.com>
2015-09-06 01:01:51 +0800

    Write starred repos to JSON file

--- starnote/starnote.py
+++ starnote/starnote.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import requests
-import sys
+import sys, json
 
 class Starnote(object):
 
@@ -10,47 +10,55 @@ class Starnote(object):
 
         self.username = args.username
         self.req_url = 'https://api.github.com/users/%s' % self.username
+        self.starred_file = 'starred.json'
 
         print "Updating %s's list of starred repositories..." % self.username
-
-        try:
-            self.res = requests.get(self.req_url)
-        except Exception, e:
-            raise e
+        self.res = requests.get(self.req_url)
 
         if self.res.status_code == 200:
-            self.traverse_stars(self.username)
+            self.update_stars(self.username, self.starred_file)
         elif self.res.status_code == 403:
             print "Resquest denied, please wait and try again"
         else:
             print "User '%s' not found or invalid username" % self.username
 
-    def traverse_stars(self, username):
+    def update_stars(self, username, filename):
 
         self.total_pages = 1
-        stars_url = 'https://api.github.com/users/{}/starred'.format(username)
+        denied = False
+        stars_url = 'https://api.github.com/users/{}/starred?per_page=100'.format(username)
         res = requests.get(stars_url)
         filebuf = res.json()
 
         if res.links:
-            self.total_pages = int(res.links['last']['url'].split('?page=')[1])
-            print "%d pages found" % self.total_pages
+            self.total_pages = int(res.links['last']['url'].split('&page=')[1])
+            print "%d pages found (100 results per page)" % self.total_pages
 
-            sys.stdout.write("[%s]" % (" " * (self.total_pages - 1)))
+            sys.stdout.write("Progress: [%s]" % (" " * self.total_pages))
             sys.stdout.flush()
-            sys.stdout.write("\b" * (self.total_pages))
+            sys.stdout.write('\b' * (self.total_pages + 1))
+            sys.stdout.write('=')
 
             for page in range(2, self.total_pages+1):
-
-                p_url = (stars_url + '?page={1}').format(username, page)
+                p_url = (stars_url + '&page={1}').format(username, page)
                 res = requests.get(p_url)
-                filebuf.extend(res.json())
 
+                if res.status_code == 403:
+                    print "\nResquest denied, please wait and try again"
+                    denied = True
+                    break
+
+                filebuf.extend(res.json())
                 sys.stdout.write('=')
                 sys.stdout.flush()
 
             sys.stdout.write('\n')
         else:
-            print "Only 1 page found"
+            print "Only 1 page found (100 results per page)"
 
-        print len(filebuf)
+        if not denied:
+            with open(filename, 'w') as filehandle:
+                json.dump(filebuf, filehandle, indent=2)
+
+            print "Successfully updated and written in 'starred.json'"
+            print len(filebuf)
